98f34209949efee1549ed91eb10941ea74a18554
Julie Gravier <JGravier@users.noreply.github.com>
2022-08-29 16:31:49 +0200

    update with isidore

--- AFU_JE/scrapping/scrapping.R
+++ AFU_JE/scrapping/scrapping.R
@@ -4,12 +4,30 @@ library(stringr)
 library(glue)
 library(httr) # for timeout option response
 
+##### Ariadne portal: inrap reports #####
+# urlbase <- 'https://portal.ariadne-infrastructure.eu/api/search?q=&page={thispage}&range={date_requete}&sort=issued&order=desc'
 urlbase <- "https://portal.ariadne-infrastructure.eu/api/search?q=&page={thispage}&sort=_score&order=asc&publisher=Institut national de recherches archéologiques préventives (Inrap)"
 
 # l'api bug à partir de la page 1001
+# debut <- seq(-10000, 1950, 50) %>% as_tibble()
+# fin <- seq(-9951, 2001, 50) %>% as_tibble()
+# date_choisie <- tibble(value = as.character())
+# 
+# for (i in 1:nrow(debut)) {
+#   requete <- paste0(debut$value[i], "%2C", fin$value[i]) %>% as_tibble()
+#   date_choisie <- date_choisie %>%
+#     bind_rows(requete)
+# }
+# 
+# date_choisie <- date_choisie$value
+
 liste_de_results <- list()
 
-for (thispage in 1:3762){
+# for (i in 1) {
+#   
+#   date_requete <- date_choisie[i]
+  
+  for (thispage in 1:3762){
     thisurl <- glue(urlbase) %>% as.character() %>% URLencode()
     print(thisurl)
     re_test <- RETRY(
@@ -22,7 +40,8 @@ for (thispage in 1:3762){
                                   flatten = FALSE, simplifyVector = FALSE, simplifyDataFrame = FALSE, simplifyMatrix = FALSE)
     liste_de_results[[thispage]] <- results
   }
-
+#   
+# }
 
 # compilation of lists
 liste_de_results_order_desc <- liste_de_results
@@ -71,3 +90,30 @@ final_list_unique <- final_list_unique %>%
   filter(is_duplicated == FALSE)
 
 saveRDS(final_list_unique, file = "final_list.RDS")
+
+##### ISIDORE #####
+urlbase <- "https://api.isidore.science/resource/search?q=title%3Aquartier&title%3Aquartier&scope=http%3A%2F%2Fisidore.science%2Fsubject%2Fpublications&page={thispage}&output=json&replies=100&discipline=http://aurehal.archives-ouvertes.fr/subject/shs.archeo"
+
+liste_de_results <- list()
+
+for (thispage in 1:8){
+  thisurl <- glue(urlbase) %>% as.character() %>% URLencode()
+  print(thisurl)
+  re_test <- RETRY(
+    "GET",
+    url = thisurl,
+    timeout(30),
+    times = 5
+  )
+  results <- jsonlite::fromJSON(content(re_test, as = "text"), 
+                                flatten = FALSE, simplifyVector = FALSE, simplifyDataFrame = FALSE, simplifyMatrix = FALSE)
+  liste_de_results[[thispage]] <- results
+}
+
+# exportation des data en JSON
+write(x = jsonlite::toJSON(final_list), file = "export_data_scrapping_ariadne_inrap.JSON")
+
+
+
+
+
